c408c814cc4d41569cb3dec44fe4563737d22ff2
Anna-Liisa Saks <annaliisa.saks@gmail.com>
2021-10-19 13:16:13 +0300

    As a user I can see my account details [Delivers #179682940]

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express()
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const swaggerUi = require('swagger-ui-express')
+const yamljs = require('yamljs')
+const swaggerDocument = yamljs.load('docs/swagger.yaml')
+const { processTransactions } = require("./middlewares");
+
+
+require('dotenv').config()
+
+// Serve API documentation on /docs
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+// Parse request body as JSON into req.body
+app.use(express.json())
+
+
+// /endpoints
+
+app.use('/users', require('./routes/users'))
+app.use('/sessions', require('./routes/sessions'))
+app.use('/transactions', require('./routes/transactions'))
+
+mongoose.connect(process.env.MONGODB_URI, {}, function () {
+    console.log('Connected to mongoDB')
+})
+processTransactions();
+
+app.listen(process.env.PORT, function () {
+    console.log(`Listening on port ${process.env.PORT}`)
+})
